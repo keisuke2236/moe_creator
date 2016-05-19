cda4024b0d3ab28b9939ac3dc0d683baaa3fe2c0
keisukeTerada <terada@basicinc.jp>
2016-05-19 03:58:39 +0000

    ok

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @feed_items = current_user.feed_items(params[:page]).includes(:user).order(created_at: :desc)
     end
     #ifを最後に付けることでこの文を実行するかはif文の結果しだいになるとかいう設定（）
     #不要になりました
