61cee681c2dea7c3dc5a9008e87a2691b19e8fcd
keisukeTerada <terada@basicinc.jp>
2016-05-27 00:14:16 +0000

    5/27

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,21 +4,30 @@ class TagsController < ApplicationController
   end
 
   def new
-    binding.pry
     @tag = Tag.new
+    @tags = DrawTag.where(user_id: current_user.id)
   end
 
   def create
     binding.pry
     #Paramsの中から値を取得．sns_paramsでSns型に当てはまらない不要なデータ型を削除する
+    
     @tag = Tag.new(sns_params)
-    @tags.user_id = current_user.id
-    if @sns.save
-      flash[:success] = "#{@sns.name}のURLを登録しました！"
-      redirect_to :action => "edit", :id => current_user.id
+    if @tag.save
+      #タグを保存できたらdraw_Tag中間テーブルにそのタグを登録した絵師のidとタグのidをひも付けておく
+      @draw = DrawTag.new
+      @draw.user_id = current_user.id
+      @draw.tag_id = @tag.id
+      if @draw.save
+        flash[:success] = "#{@tag.name}を登録しました"
+        redirect_to request.referer
+      else
+        flash[:success] = "#{@tag.name}の絵師登録ができませんでした"
+        redirect_to request.referer
+      end
     else
-      flash[:success] = "#{@sns.name}のURLを登録できませんでした"
-      redirect_to :action => "edit", :id => current_user.id
+      flash[:success] = "#{@tag.name}のURLを登録できませんでした"
+      redirect_to request.referer
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526070823) do
+ActiveRecord::Schema.define(version: 20160526091204) do
+
+  create_table "draw_tags", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "infos", force: :cascade do |t|
     t.integer  "user_id"
